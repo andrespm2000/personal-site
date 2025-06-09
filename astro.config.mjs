4c11a6b62acf3cebe4b8c6d53f43810c677cbdaf
Andrés Perdomo Martínez <andresperdomo737@gmail.com>
2025-06-09 20:06:32 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://andrespm2000.github.io',
-    base: 'parsonal-site',
+   output: 'server',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react()],
+
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Geist",
+      cssVariable: "--font-geist",
+      fallbacks: ["Inter", "sans-serif"],
+    }]
+  },
+
+  adapter: vercel()
 });
