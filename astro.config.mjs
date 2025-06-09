062cb48a99fc90c9268c6b096ffbb93049b53bb5
Andrés Perdomo Martínez <andresperdomo737@gmail.com>
2025-06-09 19:03:28 +0200

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://andrespm2000.github.io',
-    base: '/andrespm2000.github.io',
+    base: 'parsonal-site',
 });
